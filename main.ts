86837434e2332015e500187622c7c75c6e3b3e21
Jerryzcheneo <76513416+Jerryzcheneo@users.noreply.github.com>
2020-12-24 09:26:05 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,35 @@
-let People = 0
+bluetooth.onBluetoothConnected(function () {
+    start_sending = 1
+})
+bluetooth.onBluetoothDisconnected(function () {
+    start_sending = 0
+    basic.showIcon(IconNames.No)
+})
+let no_of_person = 0
+let start_sending = 0
+basic.showString("S")
+bluetooth.startUartService()
+start_sending = 0
 basic.forever(function () {
-    People = 0
+    let People = 0
+    no_of_person = 0
     if (sonar.ping(
     DigitalPin.P10,
     DigitalPin.P11,
     PingUnit.Centimeters
     ) < 5) {
-        People += 1
-        if (sonar.ping(
-        DigitalPin.P9,
-        DigitalPin.P8,
-        PingUnit.Centimeters
-        ) < 5) {
-            People += 1
-            basic.showNumber(People)
-            basic.pause(500)
-        } else {
-            basic.showNumber(People)
-            basic.pause(500)
-        }
-    } else if (sonar.ping(
+        no_of_person += 1
+    }
+    if (sonar.ping(
     DigitalPin.P9,
     DigitalPin.P8,
     PingUnit.Centimeters
     ) < 5) {
-        People += 1
-        basic.showNumber(People)
-        basic.pause(500)
-    } else {
-        basic.showNumber(People)
-        basic.pause(500)
+        no_of_person += 1
     }
+    if (start_sending == 1) {
+        bluetooth.uartWriteNumber(no_of_person)
+    }
+    basic.showNumber(People)
+    basic.pause(200)
 })
